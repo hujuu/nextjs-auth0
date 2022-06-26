4d58fec0cab1c1d478adc7b943f4dadcc5be118d
hujuu <out.of.the.base.sfc@gmail.com>
2022-06-27 01:08:25 +0900

    Add Login & Logout to Your Application

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -14,8 +14,12 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
         </h1>
 
+        <a href="/api/auth/login">Login</a>
+
+        <a href="/api/auth/logout">Logout</a>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +60,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
